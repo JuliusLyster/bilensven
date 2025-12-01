d25a4d4483d084e13641a4b423ad58837d148d81
JuliusLyster <juliuspedersenmail@gmail.com>
2025-12-01 11:43:26 +0100

    Initial commit

---
+++ src/main/java/dk/bilensven/BilensvenApplication.java
@@ -0,0 +1,13 @@
+package dk.bilensven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilensvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilensvenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/bilensven/BilensvenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.bilensven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilensvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
