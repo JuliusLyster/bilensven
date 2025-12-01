3419395508225ba72258cf46a6aea86de306e9d1
JuliusLyster <juliuspedersenmail@gmail.com>
2025-12-01 20:53:17 +0100

    Implement base entity class with auditing fields and enable JPA auditing

--- src/main/java/dk/bilensven/BilensvenApplication.java
+++ src/main/java/dk/bilensven/BilensvenApplication.java
@@ -2,12 +2,13 @@ package dk.bilensven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BilensvenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BilensvenApplication.class, args);
 	}
-
 }
