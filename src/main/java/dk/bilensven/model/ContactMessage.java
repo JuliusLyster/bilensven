ad768e878c064b2e316ff67364d755d78dbd4844
JuliusLyster <juliuspedersenmail@gmail.com>
2025-12-03 11:31:47 +0100

    Add auditing support to ContactMessage and User entities

--- src/main/java/dk/bilensven/model/ContactMessage.java
+++ src/main/java/dk/bilensven/model/ContactMessage.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ContactMessage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
